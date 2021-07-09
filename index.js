88eef0c7b866deddbdeda2e29842e5df7d1d1ec5
jeramytrapani <jeramytrapani@gmail.com>
2021-07-08 19:42:36 -0500

    Complete

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// number 1
 
-// Write your code here
+const num1 = 4
+const num2 = 15.5
+const multiply = num1 * num2
+
+//number 2
+
+//const random = Math.floor(Math.random())+1;
+const random = Math.ceil(Math.random()*10);
+
+//number 3
+
+const num3 = 36
+const num4 = 8
+const mod = num3 % num4 
+
+//number 4
+
+const max = Math.max(5,10,15,20);
+
+
+
+    
